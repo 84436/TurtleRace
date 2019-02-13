75a6d4f1d4bd2e1e98a4d3b780a744f766114545
84436 <thien.bui.84436@gmail.com>
2019-02-14 00:18:01 +0700

    thêm cơ chế tính thời gian (chưa hoàn chỉnh)
    time.perf_counter()

--- T_AddTurtles.py
+++ T_AddTurtles.py
@@ -5,5 +5,5 @@ from _SETTINGS_ import initX, initY, trackWidth, turtleCount, turtleColors
 def AddTurtles(t):
     r = sample(range(0, turtleCount), turtleCount)
     for i in range(4):
-        t.append([Turtle(), r[i], (initX, initY - trackWidth*i - trackWidth/2), turtleColors.pop(randrange(len(turtleColors))), 0, 0, 0, False])
+        t.append([Turtle(), r[i], (initX, initY - trackWidth*i - trackWidth/2), turtleColors.pop(randrange(len(turtleColors))), 0, 0, 0, False, 0.0])
     return t
