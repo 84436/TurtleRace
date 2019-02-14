4d91bfdc262a286d1064438f752898faa85bd38d
84436 <thien.bui.84436@gmail.com>
2019-02-14 12:16:20 +0700

    fix tính thời gian; thêm phần đổi hình dạng rùa

--- T_AddTurtles.py
+++ T_AddTurtles.py
@@ -1,9 +1,33 @@
+# TẠO RÙA
+
 from turtle import *
 from random import randrange, sample
-from _SETTINGS_ import initX, initY, trackWidth, turtleCount, turtleColors
+
+from _SETTINGS_ import initX, initY, trackWidth, turtleCount, turtleColors, turtleShapes
 
 def AddTurtles(t):
-    r = sample(range(0, turtleCount), turtleCount)
-    for i in range(4):
-        t.append([Turtle(), r[i], (initX, initY - trackWidth*i - trackWidth/2), turtleColors.pop(randrange(len(turtleColors))), 0, 0, 0, False, 0.0])
+    
+    order = sample(range(0, turtleCount), turtleCount)
+    moveCount = 0
+    backCount = 0
+    stopCount = 0
+    win = False
+    runtime = 0.0
+    
+    for i in range(turtleCount):
+        position = (initX, initY - trackWidth*i - trackWidth/2)
+        color = turtleColors.pop(randrange(len(turtleColors)))
+        shape = 'Resources/' + turtleShapes.pop(randrange(len(turtleShapes))) + '.gif'
+        
+        t.append([Turtle(), order[i], position, shape, color, moveCount, backCount, stopCount, runtime, win])
+        
+        t[i][0].hideturtle()
+        t[i][0].up()
+        t[i][0].goto(0, 200)
+        t[i][0].down()
+        t[i][0].color(color)
+        addshape(shape)
+        t[i][0].shape(shape)
+        t[i][0].showturtle()
+        
     return t
--- T_MakeMove.py
+++ T_MakeMove.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # HÀM MakeMove()
 
 from turtle import *
@@ -9,36 +8,27 @@ from _SETTINGS_ import turtleBackStopBias, turtleMoveLength
 
 def MakeMove(tx, trackLength_Session):
     if tx[0].distance(tx[2]) >= trackLength_Session:
-        tx[7] = True
+        if tx[9] == False: tx[8] = perf_counter() - tx[8]
+        tx[9] = True
     else:
-        tbegin = perf_counter()
-        
         x = choice(turtleMoveLength)
         moveList = ['back', 'stop', 'fwd']
         move = choice(moveList)
-        if move == 'back' and tx[4] % 10 >= turtleBackStopBias and tx[4] % 10 <= 9 and tx[5] <= 3:
+        if move == 'back' and tx[5] % 10 >= turtleBackStopBias and tx[5] % 10 <= 9 and tx[6] <= 3:
             tx[0].pencolor(bgcolor())
             tx[0].backward(x)
-            tx[5] += 1
-            tx[0].pencolor(tx[3])
-        elif move == 'stop' and tx[4] % 10 >= turtleBackStopBias and tx[4] % 10 <= 9 and tx[6] <= 2:
             tx[6] += 1
+            tx[0].pencolor(tx[4])
+        elif move == 'stop' and tx[5] % 10 >= turtleBackStopBias and tx[5] % 10 <= 9 and tx[7] <= 2:
+            tx[7] += 1
         elif move == 'fwd':
             tx[0].forward(x)
         else:
             move = 'fwd'
             tx[0].forward(x)
-        tx[4] += 1
-        
-        tend = perf_counter()
-        
-        tx[8] += round(tend-tbegin, 5)
-        
-        # print("%d; %d; %d; %s" % (tx[4], x, tx[0].distance(tm[1]), move))
-        if (tx[4] // 10 > 0 and tx[4] % 10 == 0):
-            tx[5] = 0
+        tx[5] += 1
+        if (tx[5] // 10 > 0 and tx[5] % 10 == 0):
             tx[6] = 0
+            tx[7] = 0
             # print()
     return tx
-
-##################################################
