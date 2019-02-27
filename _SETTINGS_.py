d5b3e6c251914f9adffef8420e0d1a263ae681c0
84436 <thien.bui.84436@gmail.com>
2019-02-28 03:55:13 +0700

    thêm cài đặt tương ứng

--- _SETTINGS_.py
+++ _SETTINGS_.py
@@ -1,43 +1,52 @@
 # CÀI ĐẶT
 
 # Nhạc nền
-# Đặt tên file trong '' hoặc để None để tắt nhạc
-BGMfile = None
+# Đặt file trong thư mục "Audio" và ghi tên file (bỏ đuôi .gif) trong dấu nháy đơn
+# Đặt None để tắt nhạc
+Audio_BGM = 'ProtectorsOfTheEarth'
+Audio_Win = 'win'
+
+# Hình nền
+# Đặt file trong thư mục "Backgrounds" và ghi tên file (bỏ đuôi .gif) trong dấu nháy đơn
+# Đặt None để bỏ hình nền
+## 800x800
+BGPfile = 'bg2'
 
 # Số rùa/số đường đua
 # Phải đảm bảo số này <= số con vật có trong set đã chọn
-# (trong phần 'Hình dạng con rùa' bên dưới)
+# (trong phần "Hình dạng con rùa" bên dưới)
 # và <= số màu có trong turtleColors
 c = 4
 
-# Số lượt đua (VD: 2 = có lượt đi lượt về)
-raceCount = 3
+# Số lượt đua
+# (1=1đi, 2=1đi+1về, 3=2đi+1về, 4 =2đi+2về, v.v.)
+raceCount = 2
 
 # Đường đua
 initX = -300
 initY = 100
-trackCount = c # số rùa = số đường đua
+trackCount = c
 trackUnitLength = 20
 trackUnitCount = [10, 20, 30] # 3 mức short, medium, long
 trackWidth = 60
 trackDivPadding = 10
 
 # Rùa
-turtleCount = c # số rùa = số đường đua
+turtleCount = c
 turtleColors = ['red', 'orange', 'green', 'blue', 'violet', 'purple', 'brown', 'black']
 turtleMoveLength = [10, 15, 20]
 turtleBackStopBias = 6 # giá trị trong khoảng 0 - 10
 
 # Hình dạng con rùa
-# Tên mỗi con vật trong set = tên file bỏ đuôi .GIF
-# Tất cả con vật, bất kể set nào, đều phải được đặt trong ./Resources
+# Đặt tất cả các file trong thư mục "Characters" và ghi các tên file (bỏ đuôi .gif) vào một set
 Set1 = ['chicken', 'horse', 'pig', 'sheep']
 Set2 = ['butterfly', 'grasshopper', 'turtle', 'unicorn', 'fish']
-Set3 = []
-Set4 = []
-SetPepe = ['pepe1', 'pepe2', 'pepe3', 'pepe4']
+Set3 = ['Meu2', 'Penguin2', 'Tho2', 'Pikachu']
+Set4 = ['Moe_OwO', 'Tho_OwO', 'ThoConCho', 'Jerry', 'Totoro']
 # turtleShapes = chọn tên set con vật sẽ dùng
-turtleShapes = SetPepe
+turtleShapes = Set4
 
 # Bảng điểm
-scoreboardPadding = 20
+rankInitX = -250
+rankInitY = -150
+rankWidth = 100
