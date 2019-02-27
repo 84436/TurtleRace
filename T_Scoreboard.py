19df031ec6a2d0972825e55de0199daf7257b805
84436 <thien.bui.84436@gmail.com>
2019-02-28 03:53:33 +0700

    adapt BXH của Thành Long

--- T_Scoreboard.py
+++ T_Scoreboard.py
@@ -1,8 +1,5 @@
-# VẼ BẢNG XẾP HẠNG
-
-from turtle import *
-
-from _SETTINGS_ import initX, initY, trackCount, trackWidth, scoreboardPadding
+﻿from turtle import *
+from _SETTINGS_ import turtleCount, rankInitX, rankInitY, rankWidth
 
 def RankTurtles(t):
     rank = []
@@ -14,46 +11,53 @@ def RankTurtles(t):
     for a in range(len(initialList)):
         for b in range(len(sortedList)):
             if initialList[a] == sortedList[b]:
-                rank.append((b+1, round(initialList[a], 5)))
+                rank.append((b+1, round(initialList[a], 3)))
                 break
             
     return rank
 
 def DrawScoreboard(t):
+
+    # Lấy ranklist
     rankList = RankTurtles(t)
-    columnWidth = 100
-    speed(0.25)
     
-    up()
-    goto(initX - scoreboardPadding, initY + scoreboardPadding)
+    # Dọn dẹp và nhấc bút lên
+    clear()
+    for i in range(turtleCount):
+        t[i][0].clear()
+        t[i][0].up()
+    
+    # Vẽ nền bục
+    goto(rankInitX, rankInitY)
+    showturtle()
     down()
+    forward(turtleCount * rankWidth)
+    goto(rankInitX, rankInitY)
+    left(90)
     
-    d = [(-1,1), (1,-1), (-1,1), (1,1), (1,-1), (-1,1)]
-    for i in range(len(d)):
-        left(90*d[i][0])
-        forward(2*scoreboardPadding + trackCount*trackWidth)
-        left(90*d[i][1])
-        forward(columnWidth)
-    
-    title = ['', 'RANK', 'TIME']
-    up()
-    right(90)
-    forward(scoreboardPadding)
-    right(90)
+    # Vẽ từng bục
     mark = position()
-    for i in range(3):
-        forward(i*columnWidth + columnWidth/2)
-        write(title[i], align='center', font='Consolas')
-        goto(mark)
-    
-    goto(initX-20 + (3/2)*columnWidth, initY - 2/3*trackWidth)
-    right(90)
-    for i in range(trackCount):
-        write(rankList[i][0], align='center', font='Consolas')
-        left(90)
-        forward(columnWidth)
-        write(rankList[i][1], align='center', font='Consolas')
-        backward(columnWidth)
+    for i in range(turtleCount):
+        forward((turtleCount - rankList[i][0] + 1) * rankWidth/2)
         right(90)
-        forward(trackWidth)
+        forward(rankWidth)
+        right(90)
+        forward((turtleCount - rankList[i][0] + 1) * rankWidth/2)
+        left(180)
+    
+    # Cho từng con rùa lên bục
+    for i in range(turtleCount):
+        t[i][0].goto(rankInitX + rankWidth/2 + rankWidth*i, rankInitY + (turtleCount - rankList[i][0] + 1)*(rankWidth/2) + rankWidth/4)
+    
+    # Ghi thời gian dưới bục
+    up()
+    goto(rankInitX, rankInitY)
+    forward(rankWidth/8)
+    right(90)
+    forward(rankWidth/2)
+    for i in range(turtleCount):
+        if (i != 0): forward(rankWidth)
+        write(rankList[i][1], align='center', font='Consolas')
         
+    # Xong
+    hideturtle()
