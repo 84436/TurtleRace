8f894c15da078a997ade7491bea3a78cbe9e74f5
Thiện Bùi <thien.bui.84436@gmail.com>
2019-03-04 18:36:27 +0700

    hông còn lo rùa chạm đỉnh bục nữa :3

--- T_Victory.py
+++ T_Victory.py
@@ -3,7 +3,8 @@ from _SETTINGS_ import rank_Width
 
 def Animate_Winner(t, winner):
     t[winner][0].speed(1)
-    t[winner][0].right(90)
+    t[winner][0].setheading(90) # hướng bắc trong standard mode
+    t[winner][0].forward(rank_Width/4)
     while True:
-        t[winner][0].forward(rank_Width/8)
-        t[winner][0].backward(rank_Width/8)
+        t[winner][0].forward(10)
+        t[winner][0].backward(10)
