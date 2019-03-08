7ae3c89d33941245e46c5049a6072449e9d8c251
Thiện Bùi <thien.bui.84436@gmail.com>
2019-03-08 13:45:07 +0700

    thêm hình 'YouWin'

--- T_Victory.py
+++ T_Victory.py
@@ -1,10 +1,19 @@
 from turtle import *
-from _SETTINGS_ import rank_Width
+from _SETTINGS_ import rank_Width, YouWin
 
 def Animate_Winner(t, winner):
+    WinImage = Turtle()
+    WinImage.hideturtle()
+    addshape('Backgrounds/' + YouWin + '.gif')
+    WinImage.shape('Backgrounds/' + YouWin + '.gif')
+    WinImage.setheading(90)
+    WinImage.up()
+    WinImage.forward(125)
+    WinImage.showturtle()
+    
     t[winner][0].speed(1)
     t[winner][0].setheading(90) # hướng bắc trong standard mode
-    t[winner][0].forward(rank_Width/4)
+    t[winner][0].forward(rank_Width/8)
     while True:
         t[winner][0].forward(10)
         t[winner][0].backward(10)
